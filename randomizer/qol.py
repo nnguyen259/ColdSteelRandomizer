b7f7052cd4a51b087d53c620b020fc7a96a42b0f
Nhan Nguyen <Nhan.Nguyen@unb.ca>
2020-12-04 00:07:50 -0400

    #15 Add options to make early game easier
    Add option to increase base stat by 300HP/30DEF
    Add option to replace first Needle Shoot chest with La Forte

--- randomizer/qol.py
+++ randomizer/qol.py
@@ -11,5 +11,16 @@ def reduceSlotCost(path=None, seed=None):
                 file.seek(file.tell() - 2)
                 file.write(cost.to_bytes(2, 'little'))
 
+def replaceNeedleShoot(path=None):
+    with open(path + 'data/scripts/scena/dat_us/m2500.dat', 'r+b') as file:
+        file.seek(54260)
+        file.write((2179).to_bytes(2, 'little'))
+
+        file.seek(54336)
+        file.write(b'6')
+
+        file.seek(54341)
+        file.write(b'La Forte    ')
+
 if __name__ == "__main__":
-    reduceSlotCost('data/text/dat_us/')
+    replaceNeedleShoot('./')
